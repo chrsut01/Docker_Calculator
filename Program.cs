5e2da85827b5940e64304a8a2a5a6bfd422c3df2
chrsut01 <chrissu360@gmail.com>
2024-02-23 16:41:03 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Docker_Calculator;
+
+class Program
+{
+    static void Main()
+    {
+        using var dbContext = new CalculatorDbContext();
+        var calculator = new Calculator(dbContext);
+
+        // Example usage
+        var result = calculator.Subtract(21, 3);
+        Console.WriteLine($"Subtract Result: {result}");
+
+        var result2 = calculator.Divide(21, 3);
+        Console.WriteLine($"Divide Result: {result2}");
+    }
+}
