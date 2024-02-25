476797c3002e37b34b03940e3ed38baf9fe6251f
chrsut01 <chrissu360@gmail.com>
2024-02-25 15:47:49 +0100

    externally added files

---
+++ CalculatorDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Docker_Calculator;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace Docker_Calculator;
+public class CalculatorDbContext : DbContext
+{
+    public CalculatorDbContext(DbContextOptions<CalculatorDbContext> options)
+        : base(options)
+    {
+    }
+    
+    public CalculatorDbContext()
+    {
+       
+    }
+
+    public virtual DbSet<DataLog> DataLogs { get; set; }
+
+}
